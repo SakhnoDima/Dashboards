7e6160762440c698f231e1bebaacc107bd856e0b
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-04-24 11:28:23 +0300

    change prompt

--- src/components/commands/commands.jsx
+++ src/components/commands/commands.jsx
@@ -8,15 +8,11 @@ export const customCommands = [
   `Create chart with 1 series use Название кампании and Сумма затрат (USD) add plotLines to yAxis, value 80`,
   `Create bar chart with use Название кампании and Частота, Название кампании and Сумма затрат (USD), Название кампанииand and CPC (цена за клик по ссылке)`,
 
-  `Create line chart with 1 series use Название группы объявлений and CPC (цена за клик по ссылке), Название группы объявлений and CTR (кликабельность)`,
+  `Create line chart with 1 series use Название кампании and CPC (цена за клик по ссылке), Название кампании and CTR (кликабельность)`,
 
-  `Create line chart with 1 series use Название группы объявлений and Охват, Название группы объявлений and Сумма затрат (USD)`,
+  `Create line chart with 1 series use Название кампании and Охват, Название кампании and Сумма затрат (USD)`,
 
-  "Create two widgets use Название объявления and Охват, Название объявления and Показы for one and Название объявления and Частота, Название объявления and Сумма затрат (USD) for second",
-  //`Create area chart use Название группы объявлений and Охват, Название группы объявлений and Показы`,
-  // `Create line chart use Название группы объявлений and Показы, Название группы объявлений and Клики по ссылке`,
-  // `Create pie chart use Название группы объявлений and Сумма затрат (USD)`,
-  // `Create chart use Начало and Охват`,
+  "Create two widgets use Название кампании and Охват, Название кампании and Показы for one and Название кампании and Частота, Название кампании and Сумма затрат (USD) for second",
 ];
 
 export const Commands = ({ setWidget, setLoading, loading, rootData }) => {
