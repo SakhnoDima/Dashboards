be865ea8a804ba3d4ad24a4b70473ca2f0a94814
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-04-25 17:04:36 +0300

    change prompts

--- src/components/commands/commands.jsx
+++ src/components/commands/commands.jsx
@@ -4,13 +4,13 @@ import { widget_creator } from "../../services";
 import { Box, Button } from "./commands_styled";
 
 export const customCommands = [
-  `Create pie chart use Campaign Name and Cost (USD)`,
+  `Create bar chart with use Campaign Name and Cost (USD), Campaign Name and Clicks`,
 
   `Show two line graphs. The first will show how CPC changed by Campaign Name, and the second will show how Clicks changed by Campaign Name`,
 
   `Create chart with 1 series use Campaign Name and Cost (USD). Add plotLines to yAxis, text: "Max value", value 80"`,
 
-  `Create bar chart with use "Campaign Name" and "Frequency", "Campaign Name" and Cost (USD), "Campaign Name" and "CPC"`,
+  `Create vertical bar chart with use Campaign Name and Coverage, Campaign Name and Impressions`,
 
   `Create line chart with 1 series use Campaign Name and Coverage, Campaign Name and Cost (USD)`,
 
