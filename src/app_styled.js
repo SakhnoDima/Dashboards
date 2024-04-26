4f7849e4eac254111f5c7ee801891fafddadc5fa
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-04-26 15:48:28 +0300

    bug fix

--- src/app_styled.js
+++ src/app_styled.js
@@ -3,9 +3,5 @@ import styled from "styled-components";
 export const MainBox = styled.div`
   padding: 40px 20px;
   display: flex;
-  gap: 80px;
-
-  @media (max-width: 1140px) {
-    gap: 20px;
-  }
+  gap: 20px;
 `;
